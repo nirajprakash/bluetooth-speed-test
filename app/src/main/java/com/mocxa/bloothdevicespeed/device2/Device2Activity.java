3e2a4f3206ce63ec3f2beb019746d3a7bdba6c1b
nirajprakash <nirajprakash13@gmail.com>
2022-01-12 11:16:22 +0530

    added log

--- app/src/main/java/com/mocxa/bloothdevicespeed/device2/Device2Activity.java
+++ app/src/main/java/com/mocxa/bloothdevicespeed/device2/Device2Activity.java
@@ -289,10 +289,13 @@ public class Device2Activity extends AppCompatActivity {
                         mReadBytes += msg.arg1;
                         ByteArrayOutputStream byteArrayOutputStream = (ByteArrayOutputStream) msg.obj;
                         byte[] readBuf = byteArrayOutputStream.toByteArray();
+                        log.i( "read Message 1:");
 
                         //lastMessage = new String(readBuf, 0, msg.arg1);
 
                         if (readBuf != null && mBluetoothService != null) {
+                            log.i( "read Message 2:");
+
 //                            Log.d("Live Graph", "");
                             mBluetoothService.onBytes(readBuf);
 //                            packetQueue.add(readBuf);
